c5cd05640a83c633d6b4476ee53c598e6d378acf
hz050504 <hz050504@163.com>
2022-10-20 13:53:00 +1000

    Update config.php
    Create SQL Table

--- src/config.php
+++ src/config.php
@@ -13,4 +13,18 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
+// sql to create table
+$sql = "CREATE TABLE users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    );";
+    
+    if ($conn->query($sql) === TRUE) {
+      echo "Table MyGuests created successfully";
+    } else {
+      echo "Error creating table: " . $conn->error;
+    }
 ?>
